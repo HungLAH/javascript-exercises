759e3b98522804e6780a9f75ec62e439abd89c29
Hung Pham <hphamtv@gmail.com>
2022-08-21 18:23:16 +1000

    Complete 09-palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Remove all punctuation and make it case insensitive
+  const clean_str = str.replace(/\W/g, "").toLowerCase();
+  
+  return clean_str === clean_str.split("").reverse().join("");
 };
 
 // Do not edit below this line
