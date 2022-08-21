c929741bf3f5d408aa8d44bf509fd06e4e2ca0f9
Hung Pham <hphamtv@gmail.com>
2022-08-21 19:55:07 +1000

    Complete 12-findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+// const findTheOldest = function(people) {
+//   return people.reduce((oldest, person) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+//     return personAge > oldestAge ? person : oldest;
+//   });
+// };
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const age = getAge(person.yearOfBirth, person.yearOfDeath);
+    if (oldest.name) {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      return age > oldestAge ? person : oldest;
+    }
+    else {
+      return person;
+    }
+  }, {});
 };
+
+const getAge = function(birth, death) {
+  if (!death) {
+    death = (new Date()).getFullYear();
+  }
+  return death - birth;
+}
+
+/* My sort solution */
+// const findTheOldest = function(people) {
+//   return people.sort((a, b) => {
+//     let aAge = (!a.yearOfDeath) ? (new Date()).getFullYear() - a.yearOfBirth :
+//         a.yearOfDeath - a.yearOfBirth;
+//     let bAge = (!b.yearOfDeath) ? (new Date()).getFullYear() - b.yearOfBirth :
+//         b.yearOfDeath - b.yearOfBirth;
+
+//     return aAge > bAge ? -1 : 1;
+//   })[0];
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
