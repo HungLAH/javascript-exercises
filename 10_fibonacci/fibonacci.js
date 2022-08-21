a62c35b5c4dd9e3cc5530e7975447baf6344cbf5
Hung Pham <hphamtv@gmail.com>
2022-08-21 18:52:58 +1000

    Complete 10-fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  let a = 0, b = 1, temp;
+  for (let i = 0; i < num; i++) {
+    temp = a;
+    a = a + b;
+    b = temp;
+  }
+  return a;
 };
 
 // Do not edit below this line
